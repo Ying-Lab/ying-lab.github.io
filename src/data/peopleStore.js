1d16963300500cb8a718d287ff516c535eae00af
BlankAnswer <2452841017@qq.com>
2026-09-08 11:40:28 +0800

    update:2026 students and graduated students

--- src/data/peopleStore.js
+++ src/data/peopleStore.js
@@ -105,7 +105,7 @@ const members = ref([
   {
     category: '博士生',
     name: '张家涛',
-    role: '2026级博士生',
+    role: '2027级博士生',
     description: '研究方向：图神经网络、智能算法',
     imageUrl: zhangjiataoPhoto,
   },
